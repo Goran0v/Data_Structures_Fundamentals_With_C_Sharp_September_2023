0130685b7fe58e6074d28de54de09468a78e3c8f
Lyudmil Goranov <102062855+Goran0v@users.noreply.github.com>
2025-03-21 21:29:19 +0200

    Add the exercises

---
+++ Linear Data Structures Exercise/03. Linear-Data-Structures-Exercise-Skeleton/01.FasterQueue/CircularQueue.cs
@@ -0,0 +1,87 @@
+﻿namespace Problem01.CircularQueue
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+
+    public class CircularQueue<T> : IAbstractQueue<T>
+    {
+        private T[] elements;
+        private int startIndex, endIndex;
+
+        public CircularQueue(int capacity = 4)
+        {
+            this.elements = new T[capacity];
+        }
+
+        public int Count { get; private set; }
+
+        public T Dequeue()
+        {
+            if (this.Count < 1)
+            {
+                throw new InvalidOperationException();
+            }
+
+            var element = this.elements[startIndex];
+            this.startIndex++;
+            this.Count--;
+
+            return element;
+        }
+
+        public void Enqueue(T item)
+        {
+            if (this.Count >= this.elements.Length)
+            {
+                this.Grow();
+            }
+
+            this.elements[this.endIndex] = item;
+            this.endIndex = (this.endIndex + 1) % this.elements.Length;
+            this.Count++;
+        }
+
+        public T Peek()
+        {
+            if (this.Count < 1)
+            {
+                throw new InvalidOperationException();
+            }
+
+            return this.elements[this.startIndex];
+        }
+
+        public T[] ToArray()
+        {
+            return this.CopyElements(new T[this.Count]);
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = 0; i < this.Count; i++)
+            {
+                yield return this.elements[(this.startIndex + i) % this.elements.Length];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => this.GetEnumerator();
+
+        private void Grow()
+        {
+            this.elements = this.CopyElements(new T[this.elements.Length * 2]);
+            this.startIndex = 0;
+            this.endIndex = this.Count;
+        }
+
+        private T[] CopyElements(T[] resultArr)
+        {
+            for (int i = 0; i < this.Count; i++)
+            {
+                resultArr[i] = this.elements[(this.startIndex + i) % this.elements.Length];
+            }
+
+            return resultArr;
+        }
+    }
+}
