c6f53766389a30c6f162baab9dee510d2fbacaab
Lyudmil Goranov <102062855+Goran0v@users.noreply.github.com>
2025-03-21 21:40:58 +0200

    Add the exercises

---
+++ Exam Prep/Exam Prep/Exam.AirlinesManager/AirlinesManager.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Exam.DeliveriesManager
+{
+    public class AirlinesManager : IAirlinesManager
+    {
+        private Dictionary<string, Airline> airlines = new Dictionary<string, Airline>();
+        private Dictionary<string, Flight> flights = new Dictionary<string, Flight>();
+
+        public void AddAirline(Airline airline)
+        {
+            this.airlines.Add(airline.Id, airline);
+        }
+
+        public void AddFlight(Airline airline, Flight flight)
+        {
+            if (!this.airlines.ContainsKey(airline.Id))
+            {
+                throw new ArgumentException();
+            }
+
+            this.airlines[airline.Id].Flights.Add(flight);
+            this.flights.Add(flight.Id, flight);
+        }
+
+        public bool Contains(Airline airline)
+        {
+            return this.airlines.ContainsKey(airline.Id);
+        }
+
+        public bool Contains(Flight flight)
+        {
+            return this.flights.ContainsKey(flight.Id);
+        }
+
+        public void DeleteAirline(Airline airline)
+        {
+            if (!this.airlines.ContainsKey(airline.Id))
+            {
+                throw new ArgumentException();
+            }
+
+            foreach (var flight in this.airlines[airline.Id].Flights)
+            {
+                this.flights.Remove(flight.Id);
+            }
+
+            this.airlines.Remove(airline.Id);
+        }
+
+        public IEnumerable<Airline> GetAirlinesOrderedByRatingThenByCountOfFlightsThenByName()
+        {
+            return this.airlines
+                .Values
+                .OrderByDescending(a => a.Rating)
+                .ThenByDescending(a => a.Flights.Count())
+                .ThenBy(a => a.Name);
+        }
+
+        public IEnumerable<Airline> GetAirlinesWithFlightsFromOriginToDestination(string origin, string destination)
+        {
+            return this.airlines
+                .Values
+                .Where(a => a.Flights
+                .Any(f => f.Origin == origin 
+                && f.Destination == destination
+                && !f.IsCompleted));
+        }
+
+        public IEnumerable<Flight> GetAllFlights()
+        {
+            return this.flights.Values;
+        }
+
+        public IEnumerable<Flight> GetCompletedFlights()
+        {
+            return this.flights.Values.Where(f => f.IsCompleted);
+        }
+
+        public IEnumerable<Flight> GetFlightsOrderedByCompletionThenByNumber()
+        {
+            return this.flights
+                .Values
+                .OrderBy(f => f.IsCompleted)
+                .ThenBy(f => f.Number);
+        }
+
+        public Flight PerformFlight(Airline airline, Flight flight)
+        {
+            if (!this.flights.ContainsKey(flight.Id) || !this.airlines.ContainsKey(airline.Id)) 
+            {
+                throw new ArgumentNullException();
+            }
+
+            this.flights[flight.Id].IsCompleted = true;
+            return this.flights[flight.Id];
+        }
+    }
+}
---
+++ Exam Prep/Exam Prep/Exam.DeliveriesManager/DeliveriesManager.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Exam.DeliveriesManager
+{
+    public class DeliveriesManager : IDeliveriesManager
+    {
+        private Dictionary<string, Deliverer> deliverersById = new Dictionary<string, Deliverer>();
+        private Dictionary<string, Package> packagesById = new Dictionary<string, Package>();
+        private Dictionary<string, string> packagesByDeliverer = new Dictionary<string, string>();
+
+        public void AddDeliverer(Deliverer deliverer)
+        {
+            this.deliverersById.Add(deliverer.Id, deliverer);
+        }
+
+        public void AddPackage(Package package)
+        {
+
+            this.packagesById.Add(package.Id, package);
+        }
+
+        public void AssignPackage(Deliverer deliverer, Package package)
+        {
+            if (!this.deliverersById.ContainsKey(deliverer.Id) || !this.packagesById.ContainsKey(package.Id))
+            {
+                throw new ArgumentException();
+            }
+
+            packagesByDeliverer.Add(package.Id, deliverer.Id);
+        }
+
+        public bool Contains(Deliverer deliverer)
+        {
+            return this.deliverersById.ContainsKey(deliverer.Id);
+        }
+
+        public bool Contains(Package package)
+        {
+            return this.packagesById.ContainsKey(package.Id);
+        }
+
+        public IEnumerable<Deliverer> GetDeliverers()
+        {
+            return this.deliverersById.Values;
+        }
+
+        public IEnumerable<Deliverer> GetDeliverersOrderedByCountOfPackagesThenByName()
+        {
+            var deliverersByPackageId = new Dictionary<string, int>();
+
+            foreach (var package in this.packagesByDeliverer)
+            {
+                if (!deliverersByPackageId.ContainsKey(package.Value))
+                {
+                    deliverersByPackageId.Add(package.Value, 0);
+                }
+
+                deliverersByPackageId[package.Value] += 1;
+            }
+
+            return deliverersByPackageId
+                .OrderByDescending(d => d.Value)
+                .ThenBy(d => this.deliverersById[d.Key].Name)
+                .Select(d => this.deliverersById[d.Key]);
+        }
+
+        public IEnumerable<Package> GetPackages()
+        {
+            return this.packagesById.Values;
+        }
+
+        public IEnumerable<Package> GetPackagesOrderedByWeightThenByReceiver()
+        {
+            return packagesById.Values
+                .OrderByDescending(p => p.Weight)
+                .ThenBy(p => p.Receiver);
+        }
+
+        public IEnumerable<Package> GetUnassignedPackages()
+        {
+            return packagesById.Where(p => !this.packagesByDeliverer.ContainsKey(p.Key)).Select(p => p.Value);
+        }
+    }
+}
